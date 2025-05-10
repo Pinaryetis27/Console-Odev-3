24eb40c3fb8184853eb05423a8bfc56a343b65fa
Lenovo <Lenovo@LAPTOP-AK27Q08L>
2025-05-10 22:39:46 +0300

    Proje dosyası ekle.

---
+++ Console Odev 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Lütfen bir sayı giriniz :");
+        int sayi = Convert.ToInt32(Console.ReadLine())!;
+        if (sayi >= 10)
+        {
+            Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+        }
+        else
+        {
+             Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+        }
+              
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine("Girilen sayı çifttir.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı tektir.");
+        }
+
+    }
+}
